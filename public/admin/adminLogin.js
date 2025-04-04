ffeb5c4ac44b0c802ea5b36ae3fe44830556ddce
Codecrusher9229 <raghavmishra350@gmail.com>
2025-04-04 16:52:46 +0530

    Update adminLogin.js

--- public/admin/adminLogin.js
+++ public/admin/adminLogin.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
             const password = document.getElementById("password").value;
 
             try {
-                const response = await fetch("http://localhost:5000/api/admin/login", {
+                const response = await fetch("https://future-uni-voting.onrender.com/api/admin/login", {
                     method: "POST",
                     headers: { "Content-Type": "application/json" },
                     body: JSON.stringify({ username, password }),
